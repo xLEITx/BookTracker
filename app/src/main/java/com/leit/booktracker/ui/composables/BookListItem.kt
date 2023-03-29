71c1906617cd14cc238799a1c37382b1ef1d9cf1
Vladyslav Kyrychok <vladyslav.kyrychokv@gmail.com>
2023-03-29 17:22:40 +0300

    BookListItem composable minor changes

--- app/src/main/java/com/leit/booktracker/ui/composables/BookListItem.kt
+++ app/src/main/java/com/leit/booktracker/ui/composables/BookListItem.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -21,15 +22,9 @@ import com.leit.booktracker.ui.theme.BookTrackerTheme
 import com.leit.booktracker.ui.theme.blueA700
 import com.leit.booktracker.ui.theme.greenA700
 
-/* Need to show:
-    -cover
-    -title
-    -author
-    -pages count
-    -type
-    -current status (In progress/On shelf/ In wishlist) — in fact, can be done with some color accent
-    -if book in wishlist "Add button"
- */
+
+// TODO: Change the status types in when and if statements to match the data class, maybe use some enum
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,11 +32,11 @@ fun BookListItem(
     title:String,
     status:String,
     author:String,
-    @DrawableRes cover:Int,
     onIconButtonClick:() -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    @DrawableRes cover:Int? = null,
 ){
-    Card(modifier = modifier) {
+    Card(modifier = modifier.padding(horizontal = 8.dp)) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
@@ -49,7 +44,8 @@ fun BookListItem(
                 .fillMaxWidth()
         ) {
             Image(
-                painter = painterResource(id = cover),
+                //TODO: Add default image for cover
+                painter = painterResource(id = cover ?: R.drawable.ic_launcher_background),
                 contentDescription = "",
                 contentScale = ContentScale.Crop,
                 modifier = when(status){
@@ -71,7 +67,9 @@ fun BookListItem(
 
             )
             Column(
-                modifier = Modifier.padding(horizontal = 16.dp).weight(1f),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .weight(1f),
                 horizontalAlignment = Alignment.Start,
             ) {
                 Text(text = title, style = MaterialTheme.typography.headlineSmall)
@@ -92,5 +90,20 @@ fun BookListItem(
 fun BookListItemPreview(){
     BookTrackerTheme {
         BookListItem(title = "Harry Potter", status = "In wishlist", author="J.K. Rowling", cover = R.drawable.harry, onIconButtonClick = {})
+    }
+}
+
+@Composable
+@Preview(showBackground = true, widthDp = 360)
+fun BookListItemStackPreview(){
+    BookTrackerTheme {
+        Column(modifier = Modifier) {
+            BookListItem(title = "Harry Potter", status = "In wishlist", author="J.K. Rowling", cover = R.drawable.harry, onIconButtonClick = {})
+            Spacer(modifier = Modifier.height(8.dp))
+            BookListItem(title = "Bot", status = "In progress", author="Max Kidruk", onIconButtonClick = {})
+            Spacer(modifier = Modifier.height(8.dp))
+            BookListItem(title = "Tower", status = "On boolshelf", author="King", onIconButtonClick = {})
+        }
+
     }
 }
