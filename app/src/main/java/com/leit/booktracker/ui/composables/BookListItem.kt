18dfc9ea4b0f3caa074d969cd2454dbaaa1f5f0a
Vladyslav Kyrychok <vladyslav.kyrychokv@gmail.com>
2023-03-29 15:18:35 +0300

    Color schemes

--- app/src/main/java/com/leit/booktracker/ui/composables/BookListItem.kt
+++ app/src/main/java/com/leit/booktracker/ui/composables/BookListItem.kt
@@ -1,12 +1,11 @@
 package com.leit.booktracker.ui.composables
 
+import android.content.res.Configuration
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,22 +14,35 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.leit.booktracker.R
 import com.leit.booktracker.ui.theme.BookTrackerTheme
 
+/* Need to show:
+    -cover
+    -title
+    -author
+    -pages count
+    -type
+    -current status (In progress/On shelf/ In wishlist) — in fact, can be done with some color accent
+    -if book in wishlist "Add button"
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookListItem(
     title:String,
     status:String,
+    author:String,
     @DrawableRes cover:Int,
     modifier: Modifier = Modifier
 ){
-    Surface(modifier = modifier) {
+    Card(modifier = modifier) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp).fillMaxWidth()
         ) {
             Image(
                 painter = painterResource(id = cover),
@@ -41,20 +53,20 @@ fun BookListItem(
                     .clip(CircleShape)
             )
             Column(
-                modifier = Modifier,
+                modifier = Modifier.padding(horizontal = 16.dp),
                 horizontalAlignment = Alignment.Start,
             ) {
-                Text(text = title, style = MaterialTheme.typography.labelMedium)
-                Text(text = status, style = MaterialTheme.typography.bodySmall)
+                Text(text = title, style = MaterialTheme.typography.headlineSmall)
+                Text(text = author, style = MaterialTheme.typography.labelMedium)
             }
         }
     }
 }
 
 @Composable
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 360)
 fun BookListItemPreview(){
     BookTrackerTheme {
-        BookListItem(title = "Harry Potter", status = "In Progress", cover = R.drawable.harry)
+        BookListItem(title = "Harry Potter", status = "In Progress", author="J.K. Rowling", cover = R.drawable.harry)
     }
 }
