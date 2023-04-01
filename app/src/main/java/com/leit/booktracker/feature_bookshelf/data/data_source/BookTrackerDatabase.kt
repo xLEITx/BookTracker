5feddf05ac390e26c1ff0822b9fcdb3b92f61c6b
Vladyslav Kyrychok <vladyslav.kyrychokv@gmail.com>
2023-04-01 12:26:48 +0300

    BookTrackerDatabase.kt added

---
+++ app/src/main/java/com/leit/booktracker/feature_bookshelf/data/data_source/BookTrackerDatabase.kt
@@ -0,0 +1,36 @@
+package com.leit.booktracker.feature_bookshelf.data.data_source
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.leit.booktracker.feature_bookshelf.domain.model.Book
+import com.leit.booktracker.feature_bookshelf.domain.model.ReadingSession
+
+@Database(entities = [
+        Book::class,
+        ReadingSession::class
+    ],
+    version = 1
+)
+abstract class BookTrackerDatabase: RoomDatabase() {
+
+    abstract val bookTrackerDao:BookTrackerDao
+
+/*    companion object{
+        @Volatile
+        private var INSTANCE:BookTrackerDatabase? = null
+
+        fun getInstance(context:Context):BookTrackerDatabase{
+            synchronized(this){
+                return INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    BookTrackerDatabase::class.java,
+                    "book_tracker_db"
+                ).build().also {
+                    INSTANCE = it
+                }
+            }
+        }
+    } */
+}
